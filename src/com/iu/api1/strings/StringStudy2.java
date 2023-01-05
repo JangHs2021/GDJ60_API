bd05e94f6497f31902f9b70f4509f5f37b8f39af
JangHyenSeo <wkdgustj02@naver.com>
2023-01-05 17:20:20 +0900

    1

--- src/com/iu/api1/strings/StringStudy2.java
+++ src/com/iu/api1/strings/StringStudy2.java
@@ -3,8 +3,13 @@ package com.iu.api1.strings;
 public class StringStudy2 {
 
 	public static void main(String[] args) {
-		String email = "abc123@gmail.com";
+		String email = "ab@c1@23@gmail.com";
 		
+//		int idx = email.lastIndexOf("@");
+//		System.out.println(idx);
 		
+		for(int i = 0; i < email.length(); i++) {
+			System.out.println(email.charAt(i));
+		}
 	}
 }
