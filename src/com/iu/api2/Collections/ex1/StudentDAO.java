715791b476bd973a5abb0c000ed2d76ccbd42963
JangHyenSeo <wkdgustj02@naver.com>
2023-01-10 11:40:45 +0900

    1

--- src/com/iu/api2/Collections/ex1/StudentDAO.java
+++ src/com/iu/api2/Collections/ex1/StudentDAO.java
@@ -1,5 +1,7 @@
 package com.iu.api2.Collections.ex1;
 
+import java.util.StringTokenizer;
+
 public class StudentDAO {
 	private StringBuffer sb;
 	
@@ -9,6 +11,15 @@ public class StudentDAO {
 		sb.append("winter-2-86-84-75-");
 		sb.append("suji, 3, 89, 74, 87");
 		sb.append("choa, 4, 71, 25, 99");
+	}
+	
+	// 학생 정보 초기화
+	public void init() {
+		String data = this.sb.toString();
+		System.out.println(data);
+//		StringTokenizer stringTokenizer = new StringTokenizer(data);
+		
+		
 		
 	}
 }
