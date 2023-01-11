49d00624d6955d03bfcdd3033279f51e566c1564
JangHyenSeo <wkdgustj02@naver.com>
2023-01-11 09:42:59 +0900

    1

--- src/com/iu/api2/util/RandomMain.java
+++ src/com/iu/api2/util/RandomMain.java
@@ -1,7 +1,6 @@
 package com.iu.api2.util;
 
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Random;
 
 public class RandomMain {
@@ -10,17 +9,16 @@ public class RandomMain {
 		Random random = new Random();
 		HashSet<Integer> hashSet = new HashSet<>();
 		
+		while(hashSet.size() < 6) {
+			hashSet.add(random.nextInt(45) + 1);
+		}
+		System.out.println(hashSet);
+
 		// 1000원 로또 구매 1 - 45 
 		// 번호 6개 랜덤하게 추출 중복 x
-		for(int i = 0; i < 6; i++) {
-			hashSet.add(random.nextInt(44) + 1);
-		}
-		
-		Iterator<Integer> iterator = hashSet.iterator();
-		
-		while(iterator.hasNext()) {
-			Integer num = iterator.next();
-			System.out.println(num);
-		}
+//		for(int i = 0; i < 6; i++) {
+//			hashSet.add(random.nextInt(45) + 1);
+//		}
+//		System.out.println(hashSet);
 	}
 }
