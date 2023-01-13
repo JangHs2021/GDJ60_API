e994479dfa066d798bbb3e2aeefbccca781eb295
JangHyenSeo <wkdgustj02@naver.com>
2023-01-13 17:05:45 +0900

    1

---
+++ src/com/iu/api4/network/ex/servers/ServerMain.java
@@ -0,0 +1,16 @@
+package com.iu.api4.network.ex.servers;
+
+import java.util.ArrayList;
+
+public class ServerMain {
+
+	public static void main(String[] args) {
+		
+		try {
+			new StudentController().start();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
